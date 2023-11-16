d527f483e8e6b6779e73c5f2262f74b2dfa2bf41
Iago <iagomacedo159@gmail.com>
2023-11-15 23:06:28 -0300

    fix: Ajustes no plugin

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,30 +12,21 @@ new Vue({
     btnOtimizar.disabled = true;
     btnOtimizar.classList.add('disabled');
     //   const token = localStorage.getItem("token");
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluY29sb3JoYXJtb255Iiwic3ViIjoiNjUyYWZkMTMwMDdiZmI2YTVmNWY5ZmZkIiwiaWF0IjoxNjk5MzgyMTc4LCJleHAiOjE2OTk0Njg1Nzh9.3sWNwLvAJPRJlceKtQQuNPWYa7CFcM09Z269Zu7th-g";
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImNoLWZlaXJhIiwic3ViIjoiNjU1NTUzZjIxNjQwMmY2M2ZhNzM2ZDg5IiwiaWF0IjoxNzAwMDk4MTkzLCJleHAiOjE3MDA1MzAxOTN9._SBFtZ76spB7ysQlxoc_-R_fsgRdaxACli5w1VF_VIc";
 
       if (!token) {
         // window.location.href = "/login";
-        alert('Você não possui token')
+        alert('Você não possui token');
       }
-
+    
       this.$nextTick(async () => {
-        let stylesheets = document.styleSheets;
 
-        for (let i = 0; i < stylesheets.length; i++) {
-          let stylesheet = stylesheets[i];
+        const url = window.location.href;
 
-          let rules = stylesheet.cssRules;
-
-          for (let j = 0; j < rules.length; j++) {
-            let rule = rules[j];
-
-            this.css += rule.cssText;
-          }
-        }
         const obj = {
-          css: this.css,
+          url
         };
+
         const config = {
           method: "POST",
           headers: {
@@ -44,12 +35,8 @@ new Vue({
           },
           body: JSON.stringify(obj),
         };
-        // const response = await fetch(
-        //   "https://color-harmony-api.vercel.app/optimizations/style",
-        //   config
-        // );
         const response = await fetch(
-          "http://localhost:3000/optimizations/style",
+          "https://color-harmony-api.vercel.app/api-user/optimizations/mocks",
           config
         );
         const data = await response.text();
@@ -64,6 +51,48 @@ new Vue({
 
         document.head.appendChild(styleElement);
       });
+    
+      // this.$nextTick(async () => {
+      //   let stylesheets = document.styleSheets;
+
+      //   for (let i = 0; i < stylesheets.length; i++) {
+      //     let stylesheet = stylesheets[i];
+
+      //     let rules = stylesheet.cssRules;
+
+      //     for (let j = 0; j < rules.length; j++) {
+      //       let rule = rules[j];
+
+      //       this.css += rule.cssText;
+      //     }
+      //   }
+      //   const obj = {
+      //     css: this.css,
+      //   };
+      //   const config = {
+      //     method: "POST",
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //       Authorization: `Bearer ${token}`,
+      //     },
+      //     body: JSON.stringify(obj),
+      //   };
+      //   const response = await fetch(
+      //     "http://localhost:3000/optimizations/style",
+      //     config
+      //   );
+      //   const data = await response.text();
+      //   this.optimized = true;
+      //   textBtn.innerHTML = "Otimizar visual";
+      //   btnOtimizar.classList.remove('disabled');
+      //   btnOtimizar.disabled = false;
+
+      //   let styleElement = document.createElement('style');
+      //   styleElement.id = 'optimizedCss';
+      //   styleElement.innerHTML = data;
+
+      //   document.head.appendChild(styleElement);
+      // });
     },
     handleReturn() {
         document.getElementById('optimizedCss').remove();
